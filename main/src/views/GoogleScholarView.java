268cce0d2f979e11d599d1dfa22e17ff96e35569
Ernesto Vilela <ernesto.vilela@ucb.edu.bo>
2024-01-29 17:35:40 -0400

    Displaying info inside JTextArea

--- main/src/views/GoogleScholarView.java
+++ main/src/views/GoogleScholarView.java
@@ -6,37 +6,41 @@ import models.googlescholar.OrganicResult;
 import models.googlescholar.author.AuthorModel;
 import resources.Constants;
 
+import javax.swing.*;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 public class GoogleScholarView {
 
     GoogleScholarController googleScholarController = new GoogleScholarController(this,new AuthorModel());
-
+    JTextArea jTextArea = new JTextArea(40,80);
+    JScrollPane jScrollPane = new JScrollPane(jTextArea);
     public void showOrganicResults(List<OrganicResult> results){
         for(OrganicResult result:results){
-            System.out.println("==================================");
-            System.out.println("Title: ".concat(result.getTitle()));
-            System.out.println("Summary: ".concat(result.getPublicationInfo().getSummary()));
-            System.out.println("Link: "+result.getLink());
-            System.out.println("Authors:");
+            jTextArea.append("==================================\n");
+            jTextArea.append("Title: ".concat(result.getTitle()).concat("\n"));
+            jTextArea.append("Summary: ".concat(result.getPublicationInfo().getSummary()).concat("\n"));
+            jTextArea.append("Link: "+result.getLink().concat("\n"));
+            jTextArea.append("Authors:".concat("\n"));
             if (result.getPublicationInfo().getAuthors()!=null){
                 for(Author author:result.getPublicationInfo().getAuthors()){
-                    CompletableFuture<Void> futureAuthor = this.googleScholarController
+                   CompletableFuture<Void> futureAuthor = this.googleScholarController
                             .fetchDataFromAuthorApi(Constants.BASE_GOOGLE_SCHOLAR_URL,author.getAuthorId());
                     futureAuthor.thenRun(()-> this.showAuthorInfo(this.googleScholarController.getAuthorModel())).join();
                 }
             }
             else {
-                System.out.println("**No authors**");
+                jTextArea.append("**No authors**".concat("\n"));
             }
         }
+        JOptionPane.showMessageDialog(null,jScrollPane);
     }
 
     public void showAuthorInfo(AuthorModel authorModel){
-        System.out.println("- Name: "+authorModel.getAuthorInfo().getName());
-        System.out.println("- Affiliations: "+authorModel.getAuthorInfo().getAffiliations());
-        System.out.println("***********************");
+        jTextArea.append("***********************".concat("\n"));
+        jTextArea.append("- Name: "+authorModel.getAuthorInfo().getName().concat("\n"));
+        jTextArea.append("- Affiliations: "+authorModel.getAuthorInfo().getAffiliations().concat("\n"));
+
     }
 
 }
