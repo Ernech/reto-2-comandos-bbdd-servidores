3260fc04b6337b752e690039f30fdc60480b1fcb
Ernesto Vilela <ernesto.vilela@ucb.edu.bo>
2024-01-29 16:34:20 -0400

    Author model and view

--- main/src/controllers/GoogleScholarController.java
+++ main/src/controllers/GoogleScholarController.java
@@ -9,29 +9,38 @@ import java.net.http.HttpResponse;
 import java.util.concurrent.CompletableFuture;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import models.googlescholar.GoogleScholarModel;
+import models.googlescholar.author.AuthorModel;
 import views.GoogleScholarView;
 
 public class GoogleScholarController {
 
     private GoogleScholarView view;
     private GoogleScholarModel model;
+    private  HttpClient client;
+    private AuthorModel authorModel;
+
+    public GoogleScholarController(GoogleScholarView view, AuthorModel authorModel){
+        this.view = view;
+        this.authorModel = authorModel;
+        this.client= HttpClient.newHttpClient();
+    }
 
     public GoogleScholarController(GoogleScholarView view, GoogleScholarModel model) {
         this.view = view;
         this.model = model;
+        this.client = HttpClient.newHttpClient();
     }
 
     public CompletableFuture<Void> fetchDataFromApiAsync(String url){
         return CompletableFuture.runAsync(()->{
             try {
-            HttpClient client = HttpClient.newHttpClient();
             URI finalUrl = URI.create(url).resolve(String.format("search.json?engine=%s&q=%s&start=%s&num=%s&api_key=%s"
-                    ,"google_scholar","Universidad%20de%20méxico","0","10",""));
+                    ,"google_scholar","Universidad%20de%20méxico","0","10","ff6cee49548b06aba31daa65961296ba3f39210a8bff7a2a0c86c08188c7e919"));
             HttpRequest request = HttpRequest.newBuilder()
                     .uri(finalUrl)
                     .build();
 
-            HttpResponse<String> response = client.send(request,HttpResponse.BodyHandlers.ofString());
+            HttpResponse<String> response = this.client.send(request,HttpResponse.BodyHandlers.ofString());
             ObjectMapper objectMapper = new ObjectMapper();
             GoogleScholarModel googleScholarModel = objectMapper.readValue(response.body(),GoogleScholarModel.class);
             this.setModel(googleScholarModel);
@@ -41,11 +50,35 @@ public class GoogleScholarController {
         }
         });
 
+
     }
 
+    public CompletableFuture<Void> fetchDataFromAuthorApi(String url, String authorId){
+            return CompletableFuture.runAsync(()->{
+               try{
+                URI finalUrl = URI.create(url).resolve(String.format("search.json?engine=%s&author_id=%s&api_key=%s"
+                        ,"google_scholar_author",
+                        authorId,
+                        ""));
+                HttpRequest httpRequest = HttpRequest
+                        .newBuilder().uri(finalUrl).build();
+
+                    HttpResponse<String> response = this.client.send(httpRequest,HttpResponse.BodyHandlers.ofString());
+                   ObjectMapper objectMapper = new ObjectMapper();
+                   AuthorModel authorModel = objectMapper.readValue(response.body(),AuthorModel.class);
+                   System.out.println(authorModel);
+               } catch (IOException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            });
+
+
+    }
     private void setModel(GoogleScholarModel model){
         this.model = model;
     }
+    private void setAuthorModel(AuthorModel model) {this.authorModel=model;}
     public void updateView(){
         view.showOrganicResults(this.model.getOrganicResults());
     }
